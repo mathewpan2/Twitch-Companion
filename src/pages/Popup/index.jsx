5056fe87b1d09882dbe63e71e0ce5634bf63f23a
mathewpan2 <mathew.pan2@gmail.com>
2023-12-01 19:25:46 -0500

    first commit

---
+++ src/pages/Popup/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import '../../styles/styles.css'
+import Popup from './Popup';
+
+
+const container = document.getElementById('app-container');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Popup />);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {},
+  corePlugins: {
+    preflight: true,
+  },
+}
+
